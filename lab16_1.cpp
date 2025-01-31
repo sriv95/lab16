ad7a159cce4b7438244c440401b5f363a595618b
sriv95 <srivichai95@gmail.com>
2025-01-31 17:41:23 +0700

    lab16_1.cpp complete

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,36 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *ptr,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << ptr[i];
+        if((i+1)%M==0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *ptr,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        ptr[i] = (rand()%101)/100.0;
+    }
+}
+
+void findRowSum(const double *ptr,double *sum,int N,int M){
+    for(int i=0;i<N;i++){
+        sum[i]=0;
+        for(int j=0;j<M;j++){
+            sum[i]+=*(ptr+j+(M*i));
+        }
+    }
+}
+
+void findColSum(const double *ptr,double *sum,int N,int M){
+    for(int i=0;i<M;i++){
+        sum[i]=0;
+        for(int j=0;j<N;j++){
+            sum[i]+=*(ptr+i+(M*j));
+        }
+    }
+}
